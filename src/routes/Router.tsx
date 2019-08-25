533425b985fab349ab1a0e53646e5a337aaf94f2
Samuel Oechsler <samuel@oechsler.it>
2019-08-25 12:04:16 +0200

    Setup project for rewrite

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { RouterStore } from '../stores/RouterStore';
-import { FooterContainer } from '../containers/FooterContainer';
-import { TopContainer } from '../containers/TopContainer';
 
 // Route components
 import { Home } from './home/Home';
-import { Error } from './error/Error';
 
 export const Router = () => {
     return (
         <BrowserRouter>
-            <RouterStore>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route render={() => <Error code={404} />} />
-                </Switch>
-
-                <FooterContainer />
-                <TopContainer />
-            </RouterStore>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route render={() => 404} />
+            </Switch>
         </BrowserRouter>
     );
 };
